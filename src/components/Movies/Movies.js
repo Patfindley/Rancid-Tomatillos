8e15fb8eb867e114b0528ebe05b064d845b19d79
bhohnco <bryanhohn13@gmail.com>
2021-05-12 15:25:57 -0600

    Movie component start/constructor

---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Movies.css';
+
+class Movies extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      
+    }
+  }
+  
+  render() {
+    return (
+        <h2>MoviesClass</h2>
+    )
+  }
+}
+
+export default Movies; 
