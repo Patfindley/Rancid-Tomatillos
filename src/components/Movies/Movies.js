0cbd9e66dcc0a77828008a0634cd5ac4f7cbd5e0
bhohnco <bryanhohn13@gmail.com>
2021-05-12 15:52:34 -0600

    Added props to constructor

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './Movies.css';
 
 class Movies extends React.Component {
-  constructor() {
-    super();
+  constructor ({id, poster_path, backdrop_path, title, average_rating, release_date}) {
+    super({id, poster_path, backdrop_path, title, average_rating, release_date})
     this.state = {
-      
     }
+    console.log(id)
   }
   
   render() {
