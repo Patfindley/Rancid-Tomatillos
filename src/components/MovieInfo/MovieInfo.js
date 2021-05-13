162cf1a179195917f482d160aeaafeed298492b2
Pat Findley <patfindley@gmail.com>
2021-05-13 19:33:34 -0400

    Pass props for conditional rendering

--- src/components/MovieInfo/MovieInfo.js
+++ src/components/MovieInfo/MovieInfo.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import './MovieInfo.css';
 
+const oneMovie = {
+  "movie": {
+    id: 1,
+    title: "Fake Movie Title",
+    poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg",
+    backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg",
+    release_date: "2019-12-04",
+    overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!",
+    average_rating: 6,
+    genres: ["Drama"],
+    budget:63000000,
+    revenue:100853753,
+    runtime:139,
+    tagline: "It's a movie!" }}
 
-export const MovieInfo = (props) => {
+
+const MovieInfo = (props) => {
+  console.log(props, 'movieInfo')
   return (
-    <div>
+    <div className="movie-info">
       <section className='backdrop-container'>
-        <img className='backdrop' src={props.movies.backdrop_path} />
+        <img
+        className='backdrop'
+        src={oneMovie.movie.backdrop_path}
+        alt={oneMovie.movie.titles}
+        onClick={event => props.handleClick(event)}
+        />
       </section>
+      <h1>HELLOOOOOOO</h1>
+      {console.log('hi!')}
     </div>
   )
 }
+
+export default MovieInfo;
