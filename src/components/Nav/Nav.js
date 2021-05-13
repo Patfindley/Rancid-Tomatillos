a0b2eefce6cca607fe4321c2afcdc79dc73abd21
Patrick Findley <patfindley@gmail.com>
2021-05-12 21:30:07 -0400

    Start Nav component

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import './Nav.css'
+
+const Nav = () => {
+  return (
+    <header>
+      <div>
+        Rotten Tomatillos
+      </div>
+      <nav>
+        Home
+        Genre
+      </nav>
+    </header>
+  )
+}
+
+export default Nav;
