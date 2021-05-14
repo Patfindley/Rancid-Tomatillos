65c46d212c9e7cb79a1fd2b444f3fab9def7d782
Pat Findley <patfindley@gmail.com>
2021-05-14 19:32:06 -0400

    Finish API fetch for single movie functionality

--- src/APIFetch.js
+++ src/APIFetch.js
@@ -1,10 +1,22 @@
 export const getSingleMovie =(id) => {
   return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-    .then(response => response.json())
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error(`Uh oh! We can't find the movie you're looking for!`)
+      }
+    })
 }
 
 
 export const getMovies = () => {
   return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-  .then(response => response.json())
+  .then(response => {
+    if (response.ok) {
+      return response.json()
+    } else {
+      throw new Error(`Uh oh! We're in the back looking for the movies, please try again!`)
+    }
+  })
 }
