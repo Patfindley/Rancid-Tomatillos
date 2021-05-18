380b8f51c66337c3d208596a34de80ae93980cab
bhohnco <bryanhohn13@gmail.com>
2021-05-18 14:50:26 -0600

    CardContainer testing add and command

--- cypress/integration/cardContainer_spec.js
+++ cypress/integration/cardContainer_spec.js
@@ -11,15 +11,28 @@ describe('cardContainer', () => {
     })
   })
 
-  it("should render movie cards in cardContainer from API", () => {
+  it("should render movie cards in cardContainer from API data", () => {
     cy.get(".card-container").should('be.visible')
     cy.get(".card").should('be.visible')
     cy.get(".card").should('have.length', 40)
     cy.get(".card").should('have.attr', 'alt', 'Money Plane')
     cy.get(".card").should('have.attr', 'id', '694919')
-    // cy.get(".card").should('have.attr', 'alt', 'Mulan')
-    // cy.get(".card").should('have.attr', 'id', '337401')
-    // cy.contains(".card.id").should( "694919")
     });
 
+    it("Should display an error message if data is not returned from the API", () => {
+      const errorMessage = "Uh oh! We're in the back looking for the movies, please try again!";
+      cy.intercept(
+          {
+            method: "GET",
+            url: "https://rancid-tomatillos.herokuapp.com/api/v2/movies"
+          },{
+            statusCode: 500,
+            message: errorMessage
+          }
+      )
+      cy.visit("http://localhost:3000")
+          .wait(1000)
+          .get("h1").contains(errorMessage)
+    })
   });
+
