cba2510055ccff3f10cd7aac8602b17d0887d0cb
Pat Findley <patfindley@gmail.com>
2021-05-19 17:39:38 -0400

    Add home links with Route functionality

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import SearchBar from '../SearchBar/SearchBar.js'
 import './Nav.css'
+import { Link } from 'react-router-dom'
 
 const Nav = (props) => {
   return (
     <header className="navbar">
+      <Link to="/" >
       <div className='title'>
         Rotten Tomatillos
       </div>
+      </Link >
       <SearchBar className='search-bar'/>
       <nav className='nav-buttons' >
         <div className='nav-button-container' >
-          <h5 className='home-button'
-          onClick={props.returnHome} >Home</h5>
+          <Link to="/" >
+          <h5 className='home-button'>Home</h5>
+          </Link >
           <h5 className='genre-button'>Search Genre</h5>
         </div>
       </nav>
