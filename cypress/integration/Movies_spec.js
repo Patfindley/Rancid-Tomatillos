9559a474875f0fb3af11e05a92aaae7b3ae1bd17
Pat Findley <patfindley@gmail.com>
2021-05-22 16:55:03 -0400

    Add cypress test data and sad path tests

--- cypress/integration/Movies_spec.js
+++ cypress/integration/Movies_spec.js
@@ -1,9 +1,9 @@
-describe('cardContainer', () => {
+describe('Movies', () => {
   beforeEach(() => {
     cy.allMoviesPath()
   })
 
-  it("should render card class attributes", () => {
+  it('should render card class attributes', () => {
     cy.get('img').each($el => {
       cy.wrap($el).should('have.attr', 'src')
       cy.wrap($el).should('have.attr', 'alt')
@@ -11,7 +11,7 @@ describe('cardContainer', () => {
     })
   })
 
-  it("should render movie cards in cardContainer from API data", () => {
+  it('should render movie cards in cardContainer from API data', () => {
     cy.get(".card-container").should('be.visible')
     cy.get(".card").should('be.visible')
     cy.get(".card").should('have.length', 40)
@@ -19,20 +19,19 @@ describe('cardContainer', () => {
     cy.get(".card").should('have.attr', 'id', '694919')
     });
 
-    it("Should display an error message if data is not returned from the API", () => {
-      const errorMessage = "Uh oh! We're in the back looking for the movies, please try again!";
+    it('Should display an error message if data is not returned from the API', () => {
+      const errorMessage = 'Uh oh! We\'re in the back looking for the movies, please try again!';
       cy.intercept(
           {
-            method: "GET",
-            url: "https://rancid-tomatillos.herokuapp.com/api/v2/movies"
+            method: 'GET',
+            url: 'https://rancid-tomatillos.herokuapp.com/api/v2/movies'
           },{
             statusCode: 500,
             message: errorMessage
           }
       )
-      cy.visit("http://localhost:3000")
+      cy.visit('http://localhost:3000')
           .wait(1000)
-          .get("h1").contains(errorMessage)
+          .get('h1').contains(errorMessage)
     })
   });
-
