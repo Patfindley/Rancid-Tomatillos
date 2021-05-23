e814b3b6990de1a589e252f3e6f20d11daa21c2c
Pat Findley <patfindley@gmail.com>
2021-05-23 12:33:13 -0400

    Add conditional for search errors

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Movies.css';
 import { Link } from 'react-router-dom';
 
-const Movies =({ movies, filteredMovies, handleClick }) => {
+const Movies =({ movies, filteredMovies, handleClick, inputValue, renderSearchError }) => {
   if (filteredMovies.length) {
     return filteredMovies.map(movie => {
      return <Link to={`/${movie.id}`} key={movie.id}>
@@ -14,8 +14,13 @@ const Movies =({ movies, filteredMovies, handleClick }) => {
       />
        </Link>
     })
-  } else {
+  } else if (inputValue && !filteredMovies.length) {
+    console.log(movies.length, 'error movies')
+    console.log(filteredMovies.length, 'filtered')
+      return renderSearchError()
+    } else {
     return movies.map(movie => {
+      console.log(movies.length, 'movies')
      return <Link to={`/${movie.id}`} key={movie.id}>
       <img className="card {movie.id}"
       src={movie.poster_path}
